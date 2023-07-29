7804c85f7d880d3fc17d3023bda1471290295418
banabraga <rezkypancanuary@gmail.com>
2023-07-29 11:01:06 +0700

    initial commit

---
+++ src/test/java/com/buana/technical/CRUDTest/CrudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buana.technical.CRUDTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
